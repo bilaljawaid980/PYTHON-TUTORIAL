86d2de74b2c622e5a9229de6a63060517d54b58b
BILAL JAWAID <121331714+bilaljawaid980@users.noreply.github.com>
2023-06-15 00:13:55 +0500

    Add files via upload

---
+++ dataconversion.py
@@ -0,0 +1,15 @@
+# CONVERTING FLOAT TO int AND INT TO FLOAT
+A=5
+B=6.7
+print(type(A))
+print(type(B))
+
+# AFTER CONVERSION
+c=float(A)
+d=int(B)
+print(c)
+print(d)
+# now printing its tyoe
+print(type(c))
+print(type(d))
+
