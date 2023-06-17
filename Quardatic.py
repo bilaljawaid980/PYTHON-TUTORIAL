a4911abaa4a3ebe9d17478b40c26589f33863eb7
BILAL JAWAID <121331714+bilaljawaid980@users.noreply.github.com>
2023-06-17 13:48:39 +0500

    Add files via upload
    HOW TO SOLVE QUADRATIC EQUATION WITH PYTHON

---
+++ Quardatic.py
@@ -0,0 +1,7 @@
+a=int(input("ENTER VALUE OF a = "))
+b=int(input("ENTER VALUE OF b = "))
+c=int(input("ENTER VALUE OF c = "))
+print("THE VALUE OF a,b,c are:",a,b,c)
+d=(- b + (b**2 - 4*a*c )**0.5) / 2*a
+d=(- b - (b**2 - 4*a*c )**0.5) / 2*a
+print(d)
