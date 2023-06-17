3d0213d4e27c5ae3af1a1af693597ec5bd5746a0
BILAL JAWAID <121331714+bilaljawaid980@users.noreply.github.com>
2023-06-17 18:57:25 +0500

    Add files via upload

---
+++ POWER OF 2.py
@@ -0,0 +1,7 @@
+# Program to check if no is power of 2 or not
+
+n=int(input("Enter a number to check if its power of 2 or not ="))
+if n % 2==0 :
+    print("NO IS POWER OF 2")
+else:
+    print("NUMER IS NOT THE POWER OF 2")
