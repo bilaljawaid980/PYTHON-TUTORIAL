54fd76c4f538d88d80b7bd79209d51f846b63e62
BILAL JAWAID <121331714+bilaljawaid980@users.noreply.github.com>
2023-07-12 20:34:42 +0100

    Add files via upload
    MADE CALCULATOR WITH BASIC OPERATION

---
+++ calculator.py
@@ -0,0 +1,25 @@
+print("1 FOR ADDITION")
+print("2 FOR SUBRACTION")
+print("3 FOR DIVISION")
+print("4 FOR MULTIPLICATION")
+num1=int(input("ENTER YOUR FIRST NUMBER FOR CALCULATION ="))
+num2=int(input("ENTER YOUR SECOND NUMBER FOR CALCULATION ="))
+x=int(input("ENTER YOUR CHOICE ="))
+if x==1:
+    add=num1+num2
+    print("RESULT IS =",add)
+elif x==2:
+    sub=num1+num2
+    print("RESULT IS =",sub)
+elif x==3:
+    div=num1+num2
+    print("RESULT IS =",div)
+elif x==4:
+    mult=num1+num2
+    print("RESULT IS =",mult)
+    
+else:
+   print("WRONG CHOICE")
+    
+    
+    
